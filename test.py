701d04e0a06ca350e39191f2eafc2bc4d9e60d3c
Jaya Krishna Lakkoju <64889966+Jay8500@users.noreply.github.com>
2026-04-13 14:12:06 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+# A 1D Array (like a single feature: Age)
+ages = np.array([25, 30, 35, 40])
+# A 2D Array (like a table: Age and Height)
+data = np.array([[25, 175], [30, 180], [35, 165]])
+print(ages) # This tells the AI: "I have 3 rows and 2 columns"
